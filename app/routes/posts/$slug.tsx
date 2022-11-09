6aaf9961d1ad6ae8bf05dbe373b7d6f902d24e2f
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-11-09 21:40:37 +0000

    feat: add post markdown to rendered page

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,33 +2,31 @@ import { json } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
 import type { LoaderFunction } from "@remix-run/server-runtime/dist/router"
 
+import { markedSanitized } from "~/helpers/markedSanitized"
 import { getPost } from "~/models/post.server"
 
-type LoaderData = {
-  post: Awaited<ReturnType<typeof getPost>>
-}
+type LoaderData = { title: string; html: string }
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params
 
   if (slug) {
     const post = await getPost(slug)
-    return json({ post })
+    const html = markedSanitized(post?.markdown ?? "")
+
+    return json({ title: post?.title, html })
   }
 
   return null
 }
 
 export default function PostRoute() {
-  const { post } = useLoaderData<LoaderData>()
-
-  if (!post) {
-    throw new Error("PostRoute needs to deal with unexisting posts.")
-  }
+  const { title, html } = useLoaderData<LoaderData>()
 
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
     </main>
   )
 }
