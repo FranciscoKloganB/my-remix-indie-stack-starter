610be738390f74752bc9bf954a53894301c60f4f
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-11-09 22:39:00 +0000

    feat: add basic server-side error handle to post creation action

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,7 +1,8 @@
 import type { ActionFunction } from "@remix-run/node"
-import { Form } from "@remix-run/react"
-import { redirect } from "@remix-run/server-runtime"
+import { Form, useActionData } from "@remix-run/react"
+import { json, redirect } from "@remix-run/server-runtime"
 
+import { FormError } from "~/components"
 import { createPost } from "~/models/post.server"
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
@@ -13,26 +14,44 @@ export const action: ActionFunction = async ({ request }) => {
   const slug = formData.get("slug")
   const markdown = formData.get("markdown")
 
+  const errors = {
+    title: title ? null : "title is required",
+    slug: slug ? null : "slug is required",
+    markdown: markdown ? null : "markdown is required"
+  }
+
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage !== null)
+
+  if (hasErrors) {
+    return json(errors)
+  }
+
   await createPost({ title, slug, markdown })
 
   return redirect("/posts/admin")
 }
 
 export default function NewPostRoute() {
+  const errors = useActionData()
+
   return (
     <Form method="post">
       <p>
         <label>
-          Post Title: <input type="text" name="title" className={inputClassName} />
+          Post Title: {errors?.title ? <FormError>{errors.title}</FormError> : null}
+          <input type="text" name="title" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>
-          Post Slug: <input type="text" name="slug" className={inputClassName} />
+          Post Slug: {errors?.slug ? <FormError>{errors.slug}</FormError> : null}
+          <input type="text" name="slug" className={inputClassName} />
         </label>
       </p>
       <p>
-        <label htmlFor="markdown">Markdown</label>
+        <label htmlFor="markdown">
+          Markdown: {errors?.markdown ? <FormError>{errors.markdown}</FormError> : null}
+        </label>
         <textarea
           id="markdown"
           rows={20}
