5d3f70d7f1cf1eb3bb540a1613f35eb6341b5189
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-11-12 20:35:02 +0000

    fix(tailwind): move height and zindex to extend path

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,25 +23,25 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: { ...palette.colors }
-    },
-    height: {
-      "screen-1/4": "25vh",
-      "screen-1/3": "33vh",
-      "screen-2/5": "40vh",
-      "screen-1/2": "50vh",
-      "screen-3/5": "60vh",
-      "screen-2/3": "66vh",
-      "screen-4/5": "80vh"
+      colors: { ...palette.colors },
+      height: {
+        "screen-1/4": "25vh",
+        "screen-1/3": "33vh",
+        "screen-2/5": "40vh",
+        "screen-1/2": "50vh",
+        "screen-3/5": "60vh",
+        "screen-2/3": "66vh",
+        "screen-4/5": "80vh"
+      },
+      zIndex: {
+        // Use with caution - Only really useful for modals or other elements that must be above every other
+        "1k": "1000",
+        "2k": "2000"
+      }
     },
     screens: {
       // The application only grows margins beyond 1536px
       ...screens.buildTailwindScreens(screens.breakpoints)
-    },
-    zIndex: {
-      // Use with caution - Only really useful for modals or other elements that must be above every other
-      "1K": "1000",
-      "2K": "2000"
     }
   }
 }
